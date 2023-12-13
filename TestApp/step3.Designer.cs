7e5c1722b4959767c5d9ef82897739b246f13ec6
Ilgiz <ktoto32523@ya.ru>
2023-12-13 12:51:40 +0500

    Step3 сделан.

---
+++ TestApp/step3.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace TestApp
+{
+    partial class step3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            maskedTextBox1 = new MaskedTextBox();
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            button2 = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(148, 55);
+            label1.Name = "label1";
+            label1.Size = new Size(329, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Введите полис ОСАГО второго участника ДТП";
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(6, 26);
+            maskedTextBox1.Mask = "LLL 0000000000";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(238, 27);
+            maskedTextBox1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(maskedTextBox1);
+            groupBox1.Location = new Point(148, 93);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(250, 62);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Серия и номер полиса";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(674, 386);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 3;
+            button1.Text = "Далее";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(562, 386);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 4;
+            button2.Text = "Отмена";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // step3
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Name = "step3";
+            Text = "step3";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private MaskedTextBox maskedTextBox1;
+        private GroupBox groupBox1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ TestApp/step3.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace TestApp
+{
+    public partial class step3 : Form
+    {
+        public static string ins;
+        public step3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (check() == 1)
+            {
+                ins = maskedTextBox1.Text.Trim().Replace(" ", "");
+                Hide();
+                var form = new step4cs();
+                form.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Полис ОСАГО недействителен");
+            }
+
+        }
+
+        private int check()
+        {
+            int choose = 0;
+            string cmdstr = """
+                            declare @i int
+                            set @i = (select datediff(dd, I.Date_of_issue, getdate()) from Insurance I where I.Insurance = @ins)
+                            if @i >= 0 and @i <= 365
+                                select 1 as RetVal
+                            else
+                                select 0 as RetVal
+                            """;
+            using (var con = new SqlConnection(Connection.Constr.ConnectionString))
+            {
+                using (var cmd = new SqlCommand(cmdstr, con))
+                {
+                    try
+                    {
+                        cmd.Parameters.AddWithValue("@ins", maskedTextBox1.Text.Trim().Replace(" ", ""));
+                        con.Open();
+                        choose = (int)cmd.ExecuteScalar();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ошибка: {ex}");
+                    }
+                }
+            }
+            return choose;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Hide();
+            var form = new Form3();
+            form.Show();
+            Close();
+        }
+    }
+}
