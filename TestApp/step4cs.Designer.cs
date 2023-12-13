eb123264283501f8ae7a95d1d7141145f395bd18
Ilgiz <ktoto32523@ya.ru>
2023-12-13 13:38:48 +0500

    Сделал выбор второго водителя. DriverId & VehicleId отправляются в Form6/

---
+++ TestApp/step4cs.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace TestApp
+{
+    partial class step4cs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(40, 66);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(711, 220);
+            dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(655, 397);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "Далее";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(555, 397);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 2;
+            button2.Text = "Отмена";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(40, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(244, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Выберите второго участника ДТП";
+            // 
+            // step4cs
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Name = "step4cs";
+            Text = "step4cs";
+            Load += step4cs_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+        private Label label1;
+    }
+}
---
+++ TestApp/step4cs.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+
+namespace TestApp
+{
+    public partial class step4cs : Form
+    {
+        BindingSource Sbind = new BindingSource();
+
+        public step4cs()
+        {
+            InitializeComponent();
+        }
+
+        private void step4cs_Load(object sender, EventArgs e)
+        {
+            DataTable dt = Autho();
+            Sbind.DataSource = dt;
+            dataGridView1.DataSource = Sbind;
+            {
+                dataGridView1.Columns["Surname"].HeaderText = "Фамилия";
+                dataGridView1.Columns["Name"].HeaderText = "Имя";
+                dataGridView1.Columns["Patronymic"].HeaderText = "Отчество";
+                dataGridView1.Columns["Driver_licence"].HeaderText = "Водительское удостоверение";
+            }
+        }
+
+        private DataTable Autho()
+        {
+            DataTable dt = new DataTable();
+            string cmdstr = """
+                            select D.Surname, D.Name, D.Patronymic, D.Driver_licence
+                            from Driver D
+                            join Driver_Insurance DI on D.Driver_id = DI.Driver_id
+                            join Insurance I on DI.Insurance_id = I.Insurance_id
+                            where I.Insurance = @insurance
+                            """;
+            using (var con = new SqlConnection(Connection.Constr.ConnectionString))
+            {
+                using (var cmd = new SqlCommand(cmdstr, con))
+                {
+                    try
+                    {
+                        cmd.Parameters.AddWithValue("@insurance", step3.ins);
+                        con.Open();
+                        using (var rdr = cmd.ExecuteReader())
+                        {
+                            dt.Load(rdr);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ошибка: {ex}");
+                    }
+                }
+            }
+
+            return dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dl = (string)dataGridView1.CurrentRow.Cells[3].Value;
+            Autho2(dl);
+            Hide();
+            var form = new step5();
+            form.Show();
+        }
+
+        private void Autho2(string dl)
+        {
+            string cmdstr = """
+                            select D.Driver_id, I.Vehicle_id
+                            from Driver D, Insurance I
+                            where D.Driver_licence = @dl and I.Insurance = @ins
+                            """;
+            using (var con = new SqlConnection(Connection.Constr.ConnectionString))
+            {
+                using (var cmd = new SqlCommand(cmdstr, con))
+                {
+                    try
+                    {
+                        cmd.Parameters.AddWithValue("@dl", dl);
+                        cmd.Parameters.AddWithValue("@ins", step3.ins);
+                        con.Open();
+                        using (var rdr = cmd.ExecuteReader())
+                        {
+                            if (rdr.HasRows)
+                            {
+                                while (rdr.Read())
+                                {
+                                    Form6.rta.DriverId = rdr.GetInt32(0);
+                                    Form6.rta.VehicleId = rdr.GetInt32(1);
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ошибка: {ex}");
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Hide();
+            var form = new Form3();
+            form.Show();
+        }
+    }
+}
